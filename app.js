23fa143e2638daca7b4644dd704b3bbe57af9bf6
Remi Kristelijn <rkristelijn@gmail.com>
2018-01-16 10:04:48 +0000

    fixed routing and rendering

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 var express = require('express');
-
 var app = express();
-
 var port = process.env.PORT || 4200;
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views', './src/views');
-
 app.set('view engine', 'ejs');
+
+var books = [
+  {
+    title: 'War and Peace',
+    author: 'Lev Nikolayevich Tolstoy'
+  },
+  {
+    title: 'Les Miserables',
+    author: 'The other guy'
+  }
+];
+
+bookRouter.route('/')
+  .get(function (req, res) {
+    res.render('books', {
+      title: 'Hello from books',
+      nav: [
+        { link: '/books', text: 'Books' },
+        { link: '/authors', text: 'Authors' }
+      ],
+      books: books
+    });
+  });
+
+app.use('/books', bookRouter);
 
 app.get('/', function (req, res) {
   res.render('index', {
     title: 'Hello from render ejs',
-    list: ['a', 'b']
+    nav: [
+      { link: '/books', text: 'Books' },
+      { link: '/authors', text: 'Authors' }
+    ]
   });
-});
-
-app.get('/books', function (req, res) {
-  res.send('Hello Books');
 });
 
 app.listen(port, function (err) {
