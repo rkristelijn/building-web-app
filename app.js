1ee742bb405bc8bd95722b7b4b78a19ea2dfc149
Remi Kristelijn <rkristelijn@gmail.com>
2018-01-16 08:23:12 +0000

    handlebars example

--- app.js
+++ app.js
@@ -6,16 +6,25 @@ var port = process.env.PORT;
 
 app.use(express.static('public'));
 app.set('views', './src/views');
-app.set('view engine', 'jade');
 
-app.get('/', function(req, res) {
-  res.render('index');
+var handlebars = require('express-handlebars');
+app.engine('.hbs', handlebars({
+  extname: '.hbs'
+}));
+
+app.set('view engine', '.hbs');
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    title: 'Hello from render',
+    list: ['a', 'b']
+  });
 });
 
-app.get('/books', function(req, res){
+app.get('/books', function (req, res) {
   res.send('Hello Books');
 });
 
-app.listen(port, function(err) {
+app.listen(port, function (err) {
   console.log('running server on port ' + port);
 });
