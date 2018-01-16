58c211860cf10af3ab503706cf6032b690cfb848
Remi Kristelijn <rkristelijn@gmail.com>
2018-01-16 08:29:06 +0000

    ejs test

--- app.js
+++ app.js
@@ -2,21 +2,16 @@ var express = require('express');
 
 var app = express();
 
-var port = process.env.PORT;
+var port = process.env.PORT || 4200;
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 
-var handlebars = require('express-handlebars');
-app.engine('.hbs', handlebars({
-  extname: '.hbs'
-}));
-
-app.set('view engine', '.hbs');
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'Hello from render',
+    title: 'Hello from render ejs',
     list: ['a', 'b']
   });
 });
