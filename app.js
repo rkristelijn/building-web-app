609029cc00f81a911801f367a86a2bb1a3a71f9f
Remi Kristelijn <rkristelijn@gmail.com>
2018-01-16 10:38:14 +0000

    passing nav

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 4200;
-var bookRouter = require('./src/routes/books');
+
+var nav = [
+  { link: '/books', text: 'Books' },
+  { link: '/authors', text: 'Authors' }
+];
+
+var bookRouter = require('./src/routes/books')(nav);
 
 app.use(express.static('public'));
 app.set('views', './src/views');
@@ -12,10 +18,6 @@ app.use('/books', bookRouter);
 app.get('/', function (req, res) {
   res.render('index', {
     title: 'Hello from render ejs',
-    nav: [
-      { link: '/books', text: 'Books' },
-      { link: '/authors', text: 'Authors' }
-    ]
   });
 });
 
--- src/routes/books.js
+++ src/routes/books.js
@@ -12,30 +12,27 @@ var books = [
   }
 ];
 
-bookRouter.route('/')
-  .get(function (req, res) {
-    res.render('books', {
-      title: 'Hello from books',
-      nav: [
-        { link: '/books', text: 'Books' },
-        { link: '/authors', text: 'Authors' }
-      ],
-      books: books
+var router = function (nav) {
+  bookRouter.route('/')
+    .get(function (req, res) {
+      res.render('books', {
+        title: 'Hello from books',
+        nav: nav,
+        books: books
+      });
     });
-  });
 
-bookRouter.route('/:id')
-  .get(function (req, res) {
-    var id = req.params.id;
-    console.log(id);
-    res.render('book', {
-      title: 'Hello from book',
-      nav: [
-        { link: '/books', text: 'Books' },
-        { link: '/authors', text: 'Authors' }
-      ],
-      book: books[id]
+  bookRouter.route('/:id')
+    .get(function (req, res) {
+      var id = req.params.id;
+      console.log(id);
+      res.render('book', {
+        title: 'Hello from book',
+        nav: nav,
+        book: books[id]
+      });
     });
-  });
 
-module.exports = bookRouter;
+    return bookRouter;
+  }
+module.exports = router;
